469ecb5d96711880831be8bb7d7bbac91ad8e2bb
Liu <liu@calvisitor-10-105-154-40.calvisitor.1918.berkeley.edu>
2017-07-27 10:34:10 -0700

    changed connection info

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,10 @@ if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
     connection = mysql.createConnection({
-        root: 3000,
         host: 'localhost',
         user: 'root',
         password: '',
-        database: 'burgers_db',
+        database: 'burgers_db'
     });
 };
 
